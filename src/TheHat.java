9af6d8ee7b1c0519fc9877658a603071d02cb163
robbiesollie <roberto.sollie@calbaptist.edu>
2018-03-27 07:56:16 -0700

    Revert TheHat to previous version

--- src/TheHat.java
+++ src/TheHat.java
@@ -1,3 +1,5 @@
+import org.omg.PortableInterceptor.DISCARDING;
+
 import java.util.*;
 import java.awt.*;
 
@@ -13,6 +15,7 @@ public class TheHat extends Critter {
     private int peaceTime;
     private static int peaceSum;
     private static boolean charge;
+    private static int chargeDirection;
 
 
     public TheHat() {
@@ -40,10 +43,35 @@ public class TheHat extends Critter {
             if (info.getFront()  == Neighbor.OTHER) {
                 peaceTime = 0;
                 return Action.INFECT;
-            } else if (info.getFront() == Neighbor.SAME || info.getFront() == Neighbor.WALL){
-                peaceTime = 0;
+            } else if (info.getDirection() == Direction.NORTH && chargeDirection != 0) {
+                if (chargeDirection == 3) {
+                    return Action.LEFT;
+                } else {
+                    return Action.RIGHT;
+                }
+            } else if (info.getDirection() == Direction.EAST && chargeDirection != 1) {
+                if (chargeDirection == 0) {
+                    return Action.LEFT;
+                } else {
+                    return Action.RIGHT;
+                }
+            } else if (info.getDirection() == Direction.SOUTH && chargeDirection != 2) {
+                if (chargeDirection == 1) {
+                    return Action.LEFT;
+                } else {
+                    return Action.RIGHT;
+                }
+            } else if (info.getDirection() == Direction.WEST && chargeDirection != 3) {
+                if (chargeDirection == 2) {
+                    return Action.LEFT;
+                } else {
+                    return Action.RIGHT;
+                }
+            } else {
+                if (info.getFront() == Neighbor.SAME || info.getFront() == Neighbor.WALL)
+                    peaceTime = 0;
+                return Action.HOP;
             }
-            return Action.HOP;
         }
 
         if (info.getFront() == Neighbor.OTHER) {
@@ -70,6 +98,7 @@ public class TheHat extends Critter {
 
 
         if (amIFirst) {
+            killThemAll();
             setSwarm();
             amIFirst = false;
             return Color.RED;
@@ -77,22 +106,79 @@ public class TheHat extends Critter {
         return Color.ORANGE;
     }
 
-    private void setSwarm() {
+    private void killThemAll() {
         Set<TheHat> hitList = new HashSet<>();
         for (TheHat h : swarm.keySet()) {
             if (!h.movedThisRound) {
                 hitList.add(h);
-
             }
             h.movedThisRound = false;
         }
         for (TheHat h : hitList) {
             swarm.remove(h);
         }
+    }
+
+    private void setSwarm() {
         charge = false;
-        if (peaceSum / (swarm.keySet().size() + 1) > 250) {
-            charge = true;
+        charge = peaceSum / (swarm.keySet().size() + 1) > 250;
+        int north = 0, south = 0, east = 0, west = 0;
+        for (CritterInfo i : swarm.values()) {
+            for (Direction d : Direction.values()) {
+                if (getCompass(i, d) == Neighbor.EMPTY) {
+                    switch (d) {
+                        case NORTH:
+                            north++;
+                            break;
+                        case SOUTH:
+                            south++;
+                            break;
+                        case EAST:
+                            east++;
+                            break;
+                        case WEST:
+                            west++;
+                            break;
+                    }
+                }
+            }
         }
+        chargeDirection = 0;
+        int max = north;
+        if (east > max) {
+            chargeDirection = 1;
+            max = east;
+        } if (south > max) {
+            chargeDirection = 2;
+            max = south;
+        } if (west > max) {
+            chargeDirection = 3;
+        }
+    }
+
+    private Critter.Neighbor getCompass (CritterInfo i, Direction compass) {
+        int numDirection = convertCardinal(i.getDirection());
+        int numSearch = convertCardinal(compass);
+
+        if (numDirection == numSearch) {
+            return i.getFront();
+        } else if ((numDirection + 3) % 4 == numSearch) {
+            return i.getLeft();
+        } else if ((numDirection + 1) % 4 == numSearch) {
+            return i.getRight();
+        } else {
+            return i.getBack();
+        }
+    }
+
+    private int convertCardinal(Direction compass) {
+        if (compass == Direction.NORTH)
+            return 0;
+        if (compass == Direction.EAST)
+            return 1;
+        if (compass == Direction.SOUTH)
+            return 2;
+        return 3;
     }
 
 }
